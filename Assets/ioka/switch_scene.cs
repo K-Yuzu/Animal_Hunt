3bb0f31fda9d30ff8761ad79c72d42e092f38af6
DESKTOP-VEMFGLE\GA2B <yuzuki.37@icloud.com>
2026-06-01 16:27:55 +0900

    si-nn

---
+++ Assets/ioka/switch_scene.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Switch_scene: MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void change_scene()
+    {
+        SceneManager.LoadScene("TestRoom", LoadSceneMode.Single);
+    }
+}
