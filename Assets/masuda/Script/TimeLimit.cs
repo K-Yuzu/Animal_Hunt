431437ffd24714c3401ec1f75946e214f1f2fd4b
DESKTOP-6BFKQN5\GA2B <h1202516005@hiratagakuen.onmicrosoft.com>
2026-06-03 16:30:49 +0900

    iroior

---
+++ Assets/masuda/Script/TimeLimit.cs
@@ -0,0 +1,43 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.UI;
+
+public class TimeLimit : MonoBehaviour
+{
+    public float timeLimit = 60f;
+    public TextMeshProUGUI TimerText;
+    public GameObject TestPlayer;
+
+    private bool isFinished = false;
+
+    private void Update()
+    {
+     
+
+        if(timeLimit > 0)
+        {
+            timeLimit-= Time.deltaTime;
+
+            if (timeLimit < 0)
+                timeLimit = 0;
+        }
+
+        int minites = Mathf.FloorToInt(timeLimit / 60);
+        int seconds = Mathf.FloorToInt(timeLimit % 60);
+
+        TimerText.text = $"{minites:00}:{seconds:00}";
+
+        if(timeLimit <= 0 && !isFinished)
+        {
+            Debug.Log("タイムアップ");
+            isFinished = true;
+            TimeUp();
+        }
+    }
+    void TimeUp()
+    {
+        TestPlayer.transform.position = new Vector3(0f, 0f, 0f);
+    }
+
+}
