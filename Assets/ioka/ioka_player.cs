3bd1b955b084f7f8ce749bb5c238069a8880ae0c
DESKTOP-VEMFGLE\GA2B <yuzuki.37@icloud.com>
2026-06-10 15:39:04 +0900

    no message

--- Assets/ioka/ioka_player.cs
+++ Assets/ioka/ioka_player.cs
@@ -1,6 +1,4 @@
-
 using UnityEngine;
-
 
 public class ioka_Player : MonoBehaviour
 {
@@ -10,12 +8,6 @@ public class ioka_Player : MonoBehaviour
     bool OnGround = false;
     bool sya = false;
     bool isLadder = false;
-    int jump = 1;
-    public float DefGravity=10f;
-    public float MoveSpeed = 5f;
-    public float jumpspeed = 7f;
-    private float MoveX = 0.0f;
-    private float MoveY = 0.0f;
     private void Start()
     {
        rb = GetComponent<Rigidbody2D>();
@@ -24,58 +16,38 @@ public class ioka_Player : MonoBehaviour
 
    private void Update()
     {
-        //移動処理
-        if (Input.GetKey(KeyCode.D)&&sya==false)
+        if (Input.GetKey(KeyCode.D))
         {
-            //右への移動入力
-            MoveX = MoveSpeed;
             anim.SetBool("move",true);
         }
-        else if (Input.GetKey(KeyCode.A)&&sya==false)
+        else if (Input.GetKey(KeyCode.A))
         {
-            MoveX = -MoveSpeed;
             anim.SetBool("move", true);
         }
         else
         {
-            MoveX = 0f;
             anim.SetBool("move", false);
         }
         //しゃがみ
       if (Input.GetKey(KeyCode.LeftControl) && OnGround == true)
         {
-            sya = true;
-            Debug.Log("しゃがみ");
-            if (Input.GetKey(KeyCode.D))
-            {
-                //右への移動入力
-                MoveX = MoveSpeed / 2;
-            }
-            else if (Input.GetKey(KeyCode.A))
-            {
-                MoveX = -MoveSpeed / 2;
-            }
+
         }
         else
         {
-            sya = false;
+
         }
         //ジャンプ
         if (Input.GetKey(KeyCode.Space) && OnGround == true&&isLadder==false)
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpspeed);
+
         }
-
-
-        rb.linearVelocity = new Vector2(MoveX,rb.linearVelocity.y);
-        
-
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Ground"))
         {
-            Debug.Log("床");
+            //Debug.Log("ゆかゆかゆかかかかかかかあかかかかかか");
             OnGround = true;
         }
     }
@@ -94,30 +66,19 @@ public class ioka_Player : MonoBehaviour
         if (other.CompareTag("Ladder"))
         {
             isLadder= true;
-            rb.gravityScale = 0.0f;
             //Debug.Log("Ladder接触");
             
             // 上る
             if (Input.GetKey(KeyCode.W))
             {
-                Vector3 pos = transform.position;
-                pos.y += 0.1f;
-                transform.position = pos;
             }
 
             // 下る
             if (Input.GetKey(KeyCode.S))
             {
-                Vector3 pos = transform.position;
-                pos.y -= 0.1f;
-                transform.position = pos;
             }
             else
             {
-                Vector3 pos = transform.position;
-                pos.x += 0.0f;
-                transform.position = pos;
-
             }
         
 
@@ -127,15 +88,13 @@ public class ioka_Player : MonoBehaviour
     {
         if(other.CompareTag("Ladder"))
         {
-            MoveX = 0;
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0.0f);
+
         }
     }
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Ladder"))
         {
-            rb.gravityScale = DefGravity;
             
         }
         isLadder = false;
