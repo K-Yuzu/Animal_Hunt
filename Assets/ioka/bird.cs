78563a8ad8ecb674149b7750e69f79bff4a10d07
DESKTOP-VEMFGLE\GA2B <yuzuki.37@icloud.com>
2026-06-12 14:27:54 +0900

    no message

--- Assets/ioka/bird.cs
+++ Assets/ioka/bird.cs
@@ -4,28 +4,39 @@ public class bird : MonoBehaviour
 {
     [SerializeField] Transform player;
     public float speed = 5f;
-
+    public float moveDistance = 3f;  // ‰•œ‹——£
+    private float previousX;
     private Rigidbody2D rb;
     private SpriteRenderer sr;
+
+    private Vector3 startPos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb=GetComponent<Rigidbody2D>();
         sr=GetComponent<SpriteRenderer>();
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Mathf.PingPong(Time.time * speed, moveDistance);
+        float currentX = startPos.x + x;
+
+        transform.position = new Vector3(currentX, startPos.y, startPos.z);
+
+        // ©‚±‚±‚ªd—v
+        if (currentX > previousX)
+            transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+        else
+            transform.localScale = new Vector3(-0.4f, 0.4f, 0.4f);
+
+        previousX = currentX;
     }
 
     void move()
     {
-        Vector2 awayDirection = new Vector2
-            (transform.position.x - player.position.x, 0f).normalized;
-        // ”½‘Î•ûŒü‚ÖˆÚ“®
-        //rb.linearVelocity = awayDirection * speed;
-       
+
     }
 }
--- Assets/masuda/Script/Camera1.cs
+++ Assets/masuda/Script/Camera1.cs
@@ -5,14 +5,19 @@ using System.Collections.Generic;
 
 public class Camera1 : MonoBehaviour
 {
+
     [SerializeField] Transform playerTr;
     [SerializeField] Vector2 cameraMaxPos = new Vector2(5, 5);//âEÇ∆è„ÇÃå¿äEì_
     [SerializeField] Vector2 cameraMinPos = new Vector2(-5, -5);//ç∂Ç∆ÇµÇΩÇÃå¿äEì_
-    
+    [SerializeField] Vector3 offset = new Vector3(100f, 100f, 0f); 
+
+
     void Update()
     {
-        transform.position = new Vector3(Mathf.Clamp(playerTr.position.x,cameraMinPos.x,cameraMaxPos.x),
-                                           Mathf.Clamp(playerTr.position.y,cameraMinPos.y,cameraMaxPos.y),
+        Vector3 targetPos = playerTr.position + offset;
+
+        transform.position = new Vector3(Mathf.Clamp(targetPos.x,cameraMinPos.x,cameraMaxPos.x),
+                                           Mathf.Clamp(targetPos.y,cameraMinPos.y,cameraMaxPos.y),
                                            -10f);
     }
 }
