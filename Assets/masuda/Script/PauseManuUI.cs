6eb513232d1283ad43ccce98d8652b304ea4ada5
DESKTOP-6BFKQN5\GA2B <h1202516005@hiratagakuen.onmicrosoft.com>
2026-07-10 15:58:12 +0900

    aaa

--- Assets/masuda/Script/PauseManuUI.cs
+++ Assets/masuda/Script/PauseManuUI.cs
@@ -2,20 +2,43 @@ using UnityEngine;
 
 public class PauseManuUI : MonoBehaviour
 {
-    [SerializeField] private GameManager pauseManu;
+    [SerializeField] private GameObject pauseManu;
 
     //ほかのUIが開いているかを管理する
     [SerializeField] private UIManager uiManager;
 
     void Update()
     {
-        if(Input.GetKeyUp(KeyCode.Escape))
+        if(Input.GetKeyDown(KeyCode.Escape))
         {
-            //ほかのUIが開いているなら
-            if (uiManager)
+            //ポーズメニューが開いてるなら
+            if (pauseManu.activeSelf)
             {
-
+                ClosePauseMenu();
+                return ;
             }
+
+            //ほかのUIが開いているなら
+            if (uiManager.IsAnyUIOpen)
+                return;
+
+            //ポーズメニューを開く
+           
+            OpenPauseMenu();
         }
+    }
+
+    public void OpenPauseMenu()
+    {
+        pauseManu.SetActive(true);
+        UIManager.Instance.UIClosed();
+        Time.timeScale = 0f;
+    }
+
+    public void ClosePauseMenu()
+    {
+        pauseManu.SetActive(false);
+        UIManager.Instance.UIClosed();
+        Time.timeScale = 1.0f;
     }
 }
